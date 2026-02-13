f9da0b9db2474312e0ac8faa9b3b2e5a36470c59
matlab <matlab@sjt418scope014>
2026-02-13 09:06:53 +0530

    initial commit

---
+++ https:/github.com/Prithiga-tech/Jenkins-program.git/jenkins/src/jenkins/jenkins1.java
@@ -0,0 +1,13 @@
+package jenkins;
+
+public class jenkins1 {
+
+	public static void main(String[] args) {
+		System.out.println("Helloworlds");
+		System.out.println("V.Prithiga");
+		System.out.println("23MIC0108");
+	
+
+	}
+
+}
